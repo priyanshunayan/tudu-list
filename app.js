b838ab183086f182521db1c2b2e6b633d27001fd
Priyanshu Nayan <priyanshunayan456@gmail.com>
2018-01-30 23:40:54 +0530

    change design and add keypress event

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-
-
 function addItem() {
 	var listItem = document.getElementById('content');
-
 	var toAdd = document.getElementById('inputItem').value;
 	
 	//to create a new list item and show them using javascript.
@@ -13,7 +10,7 @@ function addItem() {
 
 	// to add the 'done it' button.
 	 var btn = document.createElement("button");
-	 var btnTxt = document.createTextNode('Did it!');
+	 var btnTxt = document.createTextNode( 'Done');
 	 btn.appendChild(btnTxt);
 	 document.getElementById('content').appendChild(btn);
 
@@ -26,5 +23,20 @@ function addItem() {
 
 	// to reset the value of input element.
 	document.getElementById('inputItem').value = "";
-
+	 if (toAdd==="") {
+	 	document.getElementById("content").innerHTML = "Checking if it works or not? Smart? <br><br>Go refresh the page as a punishment!"
+	 } else{
+	 	return;
+	 }
 }
+
+// to create the same effect by pressing 'enter' and clicking 'add'...
+	function handle(e) {
+		if(e.keyCode===13) {
+			addItem();
+		} else {
+			return;
+		}
+	}
+
+
